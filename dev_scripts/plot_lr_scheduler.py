33e9880c950c45be617d29a69d73e682bac7f227
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-04-14 10:33:23 +0200

    plot linear scheduler also

--- dev_scripts/plot_lr_scheduler.py
+++ dev_scripts/plot_lr_scheduler.py
@@ -21,7 +21,7 @@ rc_fonts = {
 matplotlib.rcParams.update(rc_fonts)
 
 
-def main():
+def main(scheduler, out_fp):
     """Plot learning rate"""
     optimizer = SGD([torch.tensor(1)], lr=1)
 
@@ -29,7 +29,7 @@ def main():
         None,
         OptimizationConfig(
             lr=1,
-            lr_scheduler="relora_jagged_cosine",
+            lr_scheduler=scheduler,
             lr_warmup_steps=LR_WARMUP_STEPS,
             restart_warmup_steps=RESTART_WARMUP_STEPS,
             min_lr_ratio=0.1,
@@ -51,20 +51,23 @@ def main():
         scheduler.step()
 
     ax.plot(lrs)
-    ax.set_xlabel("Step (1000s)", size=23)
+    ax.set_xlabel("Step / 1000", size=23)
     ax.set_ylabel("Learning rate multiplier", size=23)
     ax.grid(True)
     ticks = np.arange(0, MAX_STEPS + 1, step=RELORA_RESET_FREQ)
     ax.set_xticks(ticks)
     ax.set_xticklabels(ticks // 1000)
 
+    ax.set_yticks(np.arange(0, 1.01, 0.1))
+
     ax.tick_params(axis="both", labelsize=18)
 
     fig.tight_layout()
 
-    fig.savefig("graphs/lr_scheduler.pdf")
+    fig.savefig(out_fp)
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main("linear_with_warmup", "graphs/linear_scheduler.pdf")
+    main("relora_jagged_cosine", "graphs/lr_scheduler.pdf")
