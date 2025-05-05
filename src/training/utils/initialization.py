2d92eda6c374768930fbd0d7be57e4f57d029b97
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-05-05 09:36:50 +0100

    sort imports

--- src/training/utils/initialization.py
+++ src/training/utils/initialization.py
@@ -19,7 +19,6 @@ from typing import Dict, Optional, Union
 
 import lightning as L
 import torch
-import wandb
 import yaml
 from datasets import Dataset, DownloadConfig, load_dataset
 from datasets import config as datasets_config
@@ -28,8 +27,8 @@ from lightning.fabric.loggers import Logger as FabricLogger
 from lightning.fabric.utilities.rank_zero import rank_zero_only
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
-from wandb.integration.lightning.fabric import WandbLogger
 
+import wandb
 from src.config import (
     CheckpointingConfig,
     DataConfig,
@@ -41,6 +40,7 @@ from src.config import (
 )
 from src.model import PicoDecoder, PicoReLoRADecoder
 from src.training.utils.io import use_backoff
+from wandb.integration.lightning.fabric import WandbLogger
 
 warnings.filterwarnings(
     "ignore",
